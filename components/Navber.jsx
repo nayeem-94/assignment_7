4d63a801dfd2a7adf2676c32332078e3fbef8256
nayeem-94 <nayeem11720@gmail.com>
2025-09-27 20:47:59 +0600

    fix some bug

--- components/Navber.jsx
+++ components/Navber.jsx
@@ -3,8 +3,8 @@ export default function Navber() {
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex justify-between container mx-auto  items-center">
 
-                <div className="text-xl font-bold">CS-Ticket System</div>
-                <div className="flex gap-5 justify-center items-center">
+                <div className="text-xl  font-bold">CS-Ticket System</div>
+                <div className=" grid grid-cols-3 md:flex gap-5 justify-center items-center">
                     <p>Home</p>
                     <p>FAQ</p>
                     <p>Changelog</p>
@@ -12,7 +12,7 @@ export default function Navber() {
                     <p>Download</p>
                     <p>Contact</p>
 
-                    <button className="btn rounded-lg text-white text-lg bg-linear-to-r from-[#632ee3] to-[#9f62f2]">+ New Ticket</button>
+                    <button className="btn rounded-lg text-white md:text-lg bg-linear-to-r from-[#632ee3] to-[#9f62f2]">+ New Ticket</button>
 
 
                 </div>
