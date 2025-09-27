e914b60adaa6220d2818954bd1afe9f65c1216ab
nayeem-94 <nayeem11720@gmail.com>
2025-09-27 20:36:41 +0600

    added the navber section

--- components/Navber.jsx
+++ components/Navber.jsx
@@ -1,5 +1,24 @@
 export default function Navber() {
-    return(
-        <div className="bg-red-600">navber</div>
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="flex justify-between container mx-auto  items-center">
+
+                <div className="text-xl font-bold">CS-Ticket System</div>
+                <div className="flex gap-5 justify-center items-center">
+                    <p>Home</p>
+                    <p>FAQ</p>
+                    <p>Changelog</p>
+                    <p>Blog</p>
+                    <p>Download</p>
+                    <p>Contact</p>
+
+                    <button className="btn rounded-lg text-white text-lg bg-linear-to-r from-[#632ee3] to-[#9f62f2]">+ New Ticket</button>
+
+
+                </div>
+            </div>
+
+        </div>
+
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
 
   return (
     <>
-      <div className="bg-blue-600">nayeem 22222</div>
       <Navber > </Navber>
 
 
