7737133222f50cf446227915b702816184771449
nayeem-94 <nayeem11720@gmail.com>
2025-09-28 20:48:01 +0600

    added the responsivness and some small issu

--- components/User.jsx
+++ components/User.jsx
@@ -4,8 +4,10 @@ export default function User({ user }) {
         <div className='bg-white p-5 rounded-lg shadow-md my-5'>
             <div className='flex justify-between '>
                 <h1 className='font-semibold text-xl'>{user.title}</h1>
-                <div className='flex justify-center gap-2 items-center rounded-xl text-lg bg-green-200  '>
-                    <div className='w-4 h-4 rounded-full bg-green-600'></div>
+                <div className={`flex justify-center gap-2 items-center rounded-xl text-lg
+                    ${user.status=="In-Progress"?"bg-yellow-100 ": "bg-green-200 text-green-950" } `}>
+                    <div className={`w-4 h-4 rounded-full
+                            ${user.status === "In-Progress" ? "bg-yellow-300" : "bg-green-600"}`}></div>
 
                     <button className='text-green-950 '>{user.status}</button>
                 </div>
@@ -14,7 +16,7 @@ export default function User({ user }) {
             <div className='flex  flex-col md:flex-row justify-between text-[#627382]'>
                 <div className='flex justify-between gap-3'>
                     <h3>#{user.id}</h3>
-                    <p className={user.priority === "HIGH PRIORITY" ? "text-red-500" : user.priority === "LOW" ? "text-green-500" : "text-yellow-600"} >{user.priority}</p>
+                    <p  className={user.priority === "HIGH PRIORITY" ? "text-red-500 font-bold" : user.priority === "LOW PRIORITY" ? "text-green-500 font-bold" : "text-yellow-500 font-bold"} >{user.priority}</p>
                 </div>
                 <div className='flex justify-between gap-3'>
                     <h3> {user.customer} </h3>
